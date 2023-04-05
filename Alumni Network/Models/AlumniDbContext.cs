aaf252e3a0239d6adea54a5e897d975b30949086
Rubin Barclay <63505731+RubinBarclay@users.noreply.github.com>
2023-04-05 10:24:58 +0200

    feat: base entity for automatic timestamps on save

---
+++ Alumni Network/Models/AlumniDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Alumni_Network.Models
+{
+    public class AlumniDbContext : DbContext
+    {
+        public override int SaveChanges()
+        {
+            var entries = ChangeTracker.Entries().Where(e => e.Entity is BaseEntity && 
+            (
+                e.State == EntityState.Added || e.State == EntityState.Modified)
+            );
+
+            foreach (var entityEntry in entries)
+            {
+                ((BaseEntity)entityEntry.Entity).UpdatedAt = DateTime.Now;
+
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((BaseEntity)entityEntry.Entity).CreatedAt = DateTime.Now;
+                }
+            }
+
+            return base.SaveChanges();
+        }
+    }
+}
