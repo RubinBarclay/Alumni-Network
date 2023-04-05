309fc454cf808d787807b48bdfc9f149f8169f26
Rubin Barclay <63505731+RubinBarclay@users.noreply.github.com>
2023-04-05 09:39:29 +0200

    feat: added user model

---
+++ Alumni Network/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alumni_Network.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+        [Required, MaxLength(500)]
+        public string Picture { get; set; }
+        [Required, MaxLength(250)]
+        public string Status { get; set; }
+        [Required, MaxLength(500)]
+        public string Bio { get; set; }
+        [Required, MaxLength(250)]
+        public string FunFact { get; set; }
+    }
+}
