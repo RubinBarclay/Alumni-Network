bce0ad66b097f958bd4e67175d2d151271189ac9
Rubin Barclay <63505731+RubinBarclay@users.noreply.github.com>
2023-04-17 15:16:15 +0200

    feat: created user service template

---
+++ Alumni Network/Services/User/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Alumni_Network.Models.DTOs.User;
+
+namespace Alumni_Network.Services.User
+{
+    public interface IUserService
+    {
+        public Task<GetUserDTO> GetUserAsync();
+        public Task<GetUserByIdDTO> GetUserByIdAsync();
+        public Task<CreateUserDTO> CreateUserAsync();
+        public Task<EditUserDTO> EditUserAsync();
+    }
+}
---
+++ Alumni Network/Services/User/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Alumni_Network.Models.DTOs.User;
+
+namespace Alumni_Network.Services.User
+{
+    public class UserService : IUserService
+    {
+        public Task<CreateUserDTO> CreateUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditUserDTO> EditUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetUserDTO> GetUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetUserByIdDTO> GetUserByIdAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
